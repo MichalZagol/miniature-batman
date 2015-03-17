b62aa27bf085cc99a52aa648b433b03b2bb83007
Michał Zagól <mysza1179@gmail.com>
2015-03-17 13:30:58 +0000

    edit - update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,43 @@
 class ArticlesController < ApplicationController
-   def index
+  def index
     @articles = Article.all
   end
   
-   def show
+  def show
     @article = Article.find(params[:id])
   end
   
   def new
+    @article = Article.new
   end
-    
-  def create
-  @article = Article.new(article_params)
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
  
-  @article.save
-  redirect_to @article
+  def create
+    @article = Article.new(article_params)
+ 
+  if @article.save
+    redirect_to @article
+  else
+    render 'new'
+  end
 end
  
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
 private
   def article_params
     params.require(:article).permit(:title, :text)
   end
 end
+
